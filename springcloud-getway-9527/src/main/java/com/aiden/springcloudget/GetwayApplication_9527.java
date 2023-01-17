ab1739e911f7c381e6a145020077c7d092cd20af
ning yu <17629013070@163.com>
2023-01-17 12:25:00 +0800

    spring cloud getWay 初步配置

---
+++ springcloud-getway-9527/src/main/java/com/aiden/springcloudget/GetwayApplication_9527.java
@@ -0,0 +1,13 @@
+package com.aiden.springcloudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetwayApplication_9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetwayApplication_9527.class, args);
+    }
+
+}
